44b0707da2299ff94fc9aad1807a690aed7ea0db
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-16 21:03:53 +1000

    Added api routes and static file middleware

--- server.js
+++ server.js
@@ -5,14 +5,25 @@ const PORT = process.env.PORT || 8080;
 
 const path = require('path');
 
+app.use(express.static(__dirname + "/public"));
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
